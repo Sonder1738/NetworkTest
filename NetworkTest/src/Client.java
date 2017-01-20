538873576c15cacc4fb78264e073c560e09ea26d
Sonder1738 <mc_laren_93@hotmail.com>
2017-01-20 08:58:27 +0800

    hiiiii

--- NetworkTest/src/Client.java
+++ NetworkTest/src/Client.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.net.Socket;
 
 
-//http://mrbool.com/file-transfer-between-2-computers-with-java/24516   as222aaaaasd
+//http://mrbool.com/file-transfer-between-2-computers-with-java/24516   as222whataasd
 public class Client {
 
 	public static void main(String[] args) throws IOException{
