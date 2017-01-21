a0e1b73ac8bd2e1d8c82c5db1338c8830d28fc77
Sonder1738 <mc_laren_93@hotmail.com>
2017-01-21 14:07:36 +0800

    working chat one time

--- NetworkTest/src/Client.java
+++ NetworkTest/src/Client.java
@@ -1,9 +1,13 @@
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 
@@ -16,10 +20,23 @@ public class Client {
 		
 		System.out.println("This run?");
 		Socket MyClient;
-	    MyClient = new Socket("192.168.0.8", 15678);
+	    MyClient = new Socket("127.0.0.1", 15676);
 	    
-	    
+	    BufferedReader sin=new BufferedReader(new InputStreamReader(MyClient.getInputStream()));
+		PrintStream sout=new PrintStream(MyClient.getOutputStream());
+		BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while (  true )
+		{
+			System.out.print("Client : ");
+			s=stdin.readLine();
+			sout.println(s);
+			s=sin.readLine();
+			System.out.print("Server : "+s+"\n");
+  			if ( s.equalsIgnoreCase("BYE") )
+ 			   break;
+		}
+
+}
 	
-	
-	}
 }
--- NetworkTest/src/Server.java
+++ NetworkTest/src/Server.java
@@ -1,4 +1,5 @@
-import java.io.BufferedInputStream;
+import java.io.*;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,35 +7,89 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
+	static Socket serverSocket = null;
+	
 	public static void main(String[] args) throws IOException{
 		
+		start();
 		
-
+		}
+	
+	public static void start() throws IOException{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Press 1 to start server");
+		System.out.println("Press 2 to end server");
+		System.out.println("Press 3 to start chatting");
+		System.out.println("Please select number");
+		
+		int choice;
+		choice = in.nextInt();
+		
+		switch (choice){
+		
+		case 1: startsvr();
+		break;
+		case 2: closesvr();
+		break;
+		case 3: startchat();
+		break;
+		default : System.out.println("You entered something else");
+		
+		}
+		
+	}
+	
+	public static void startsvr(){
 		ServerSocket MyService = null;
 		
 	    try {
-	       MyService = new ServerSocket(15678);
+	    	System.out.println("This server is waiting for connection");
+	    	MyService = new ServerSocket(15676);
 	        }
 	        catch (IOException e) {
 	           System.out.println(e);
 	           
 	        }
 	    
-	    Socket clientSocket = null;
+	    
 	    try {
-	    	clientSocket = MyService.accept();
-	    	System.out.println("accepted.connected");
-	        }
-	    catch (IOException e) {
+	    	serverSocket = MyService.accept();
+	    	System.out.println("Connection successful");
+	    	System.out.println("Client connected with IP: "+serverSocket.getInetAddress().getHostAddress());
+	    	
+	    	start();
+	    }catch (IOException e) {
 	       System.out.println(e);
 	    }
-	    
-	    
-	    
-	    
-		}
+	}
 
+	public static void closesvr() throws IOException{
+		serverSocket.close();
+	}
+	public static void startchat() throws IOException{
+		
+		BufferedReader cin=new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+		PrintStream cout=new PrintStream(serverSocket.getOutputStream());
+		BufferedReader stdin=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while (  true )
+		{
+			
+			s=cin.readLine();
+  			if (s.equalsIgnoreCase("END"))
+  			{
+				cout.println("BYE");
+    				break;
+  			  }
+			System. out.print("Client : "+s+"\n");
+			System.out.print("Server : ");
+			s=stdin.readLine();
+			cout.println(s);
+		}
+	}
 }
